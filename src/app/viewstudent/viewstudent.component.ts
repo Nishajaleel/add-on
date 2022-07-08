7d147d861bc04171dbd3efb35c2bcc0d63c25e6b
Nishajaleel <nishanajaleel690@gmail.com>
2022-07-08 19:11:03 +0530

    student

--- src/app/viewstudent/viewstudent.component.ts
+++ src/app/viewstudent/viewstudent.component.ts
@@ -9,7 +9,18 @@ export class ViewstudentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
- name="nisha"
+ data=[
+  {"name":"riya","age":20,"photo":"https://www.directive.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"}, 
+   {"name":"luha","age":19,"photo":"https://www.directive.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"},
+   {"name":"hanana","age":19,"photo":"https://www.directive.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"},
+   {"name":"lubna","age":19,"photo":"https://www.directive.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"},
+   {"name":"isha","age":19,"photo":"https://www.directive.com/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"}
+
+
+
+
+
+]
 }
